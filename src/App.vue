7d1569c33b6be2137a79c7770ab0f0b34de541d8
Rollein <151161534+Rollein@users.noreply.github.com>
2026-05-05 20:25:11 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Vheader from '@/Components/Vheader.vue'
+import Vfooter from '@/Components/Vfooter.vue'
+</script>
+
+<template>
+  <Vheader/>
+  <RouterView/>
+  <Vfooter/>
+</template>
+
+<style scoped></style>
