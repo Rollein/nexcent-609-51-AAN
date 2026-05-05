35cc36c660eb9ccfb08edd7648f656d53df29016
Rollein <151161534+Rollein@users.noreply.github.com>
2026-05-05 20:28:45 +0500

    Create index.js

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import homeview from '../views/homeview.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [    {
+      path: '/',
+      name: 'home',
+      component: homeview,
+    },],
+})
+
+export default router
