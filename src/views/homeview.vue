a9c6be75a8cc740e8eb2ccfb696fe08776b99dc5
Rollein <151161534+Rollein@users.noreply.github.com>
2026-05-05 20:28:02 +0500

    Create homeview.vue

---
+++ src/views/homeview.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Vpromo from '@/Components/Vpromo.vue'
+import Vclients from '@/Components/Vclients.vue'
+import Vmanage from '@/Components/Vmanage.vue'
+import Vachievements from '@/Components/Vachievements.vue'
+import Vcustomer from '@/Components/Vcustomer.vue'
+</script>
+
+<template>
+  <main>
+    <Vpromo />
+    <Vclients />
+    <Vmanage />
+    <Vachievements />
+    <Vcustomer />
+  </main>
+</template>
